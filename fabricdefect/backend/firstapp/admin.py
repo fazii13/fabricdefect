815e1a286b2157f46768db97d8e43765832f4179
fazi13 <chfaizanmahmood13@gmail.com>
2025-10-04 14:03:10 +0300

    Updated

--- fabricdefect/backend/firstapp/admin.py
+++
@@ -1,7 +0,0 @@
-# firstapp/admin.py
-
-from django.contrib import admin
-from .models import React
-
-# Register your models here.
-admin.site.register(React)
--- fabricdefect/backend/firstapp/backend_logic/test.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(r"backend/firstapp/backend_logic/best.pt")
-results = model(source=0, show=True, conf=0.4, save=True,stream=False)
